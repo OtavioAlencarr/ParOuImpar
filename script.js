6ec0c5bdf17608f9a348323378335848ce4381b4
Otavio Alencar <oalencar32@gmail.com>
2021-11-03 20:26:58 -0200

    aa

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-function parOuImpar(computador,usuario){
+function parOuImpar(computador,usuario,total){
     function gerarNumero(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
     computador = gerarNumero(0,10)
-    usuario = document.getElementById='numeroUsuario'.value
-    if (document.getElementById='escolha' === 'Par' && (usuario + computador) % 2 == 0)
-                document.getElementById('resultado').innerText = 'Parabéns, você venceu'
-
+    usuario = Number(document.getElementById('numeroUsuario').value)
+    total = usuario + computador
+    let ePar = document.getElementById('escolha').value === 'Par' 
+    let totalEPar = total % 2 == 0
+    let mensagem = ''
+    document.getElementById('sorteio').innerText = `O número sorteado foi ${computador}`
+    if (ePar && totalEPar){
+                mensagem = `O número ${total} é par,Parabéns, você venceu`}
+    else if (ePar && !totalEPar){
+                mensagem = `O número ${total} é ímpar, Sinto muito, você perdeu`}
+    else if (!ePar && !totalEPar){
+                mensagem = `O número ${total} é ímpar,Parabéns, você venceu`}
+    else if (!ePar && totalEPar){
+                mensagem = `O número ${total} é par, Sinto muito, você perdeu`}
+    document.getElementById('resultado').innerText = mensagem
     }
-
